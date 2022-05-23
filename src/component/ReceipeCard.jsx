d184f9ce2f8c3e96062477d44f156fcdcd6e5c0d
vivinkv6 <vivinkv0@gmail.com>
2022-05-23 11:24:47 +0530

    modify  header,card styles

--- src/component/ReceipeCard.jsx
+++ src/component/ReceipeCard.jsx
@@ -1,4 +1,4 @@
-
+import './card.css';
 function RecipeCard({values}) {
 const{strMeal,strCategory,strMealThumb,strSource}=values;
     return(
@@ -7,7 +7,7 @@ const{strMeal,strCategory,strMealThumb,strSource}=values;
 <div className="card h-50" style={{width: '25rem',marginBottom:'20px',marginRight:'20px',boxShadow:'5px 5px 10px black',display:'flex',justifyContent:'center'}} >
   <img src={strMealThumb} className="card-img-top h-50" alt="Not found"/>
   <div className="card-body">
-    <h3 className="card-title">Meal: {strMeal}</h3>
+    <h3 className="card-title">Item: <i>{strMeal}</i></h3>
     <h5 className="card-text">Category: {strCategory}</h5>
     <div style={{display:'flex',justifyContent:'center'}}>
     <a href={strSource} type='button' className="btn btn-primary" style={{padding:'10px',marginTop:'10px'}}>Ingredients</a>
